ea0f3d2b0175cfdd6e00cc41508db144e56b7187
Alberto Chirinos <developernet@outlook.com>
2021-02-24 08:27:18 -0500

    Login view creation

---
+++ Countries/Countries/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Countries.Views;
+
+namespace Countries
+{
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
